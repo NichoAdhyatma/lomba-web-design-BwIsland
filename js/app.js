3e4c38c85dfe2cdd5eaa47099a7453374b53a950
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2022-10-31 22:13:10 +0700

    add-text-svg-animation

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ AOS.init();
 window.addEventListener("load", () => {
   const preloader = document.querySelector(".js-preloader");
   const app = document.querySelector("#app");
+
   app.style.display = "none";
 
   setTimeout(() => {
@@ -16,8 +17,34 @@ window.addEventListener("load", () => {
 
   setTimeout(() => {
     preloader.style.display = "none";
+    $("#container").addClass("fin");
   }, 2000);
 });
+
+function setTextAnimation(
+  delay,
+  duration,
+  strokeWidth,
+  timingFunction,
+  strokeColor,
+  repeat
+) {
+  let paths = document.querySelectorAll("path");
+  let mode = repeat ? "infinite" : "forwards";
+  for (let i = 0; i < paths.length; i++) {
+    const path = paths[i];
+    const length = path.getTotalLength();
+    path.style["stroke-dashoffset"] = `${length}px`;
+    path.style["stroke-dasharray"] = `${length}px`;
+    path.style["stroke-width"] = `${strokeWidth}px`;
+    path.style["stroke"] = `${strokeColor}`;
+    path.style[
+      "animation"
+    ] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
+    path.style["animation-delay"] = `${i * delay}s`;
+  }
+}
+setTextAnimation(0.2, 4.6, 3, "linear", "#ffffff", false);
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 /* ---- particles.js config ---- */
@@ -132,5 +159,3 @@ particlesJS("particles-js", {
   },
   retina_detect: true,
 });
-
-
